21265f069ba6003d416c41339a94ceac838d6027
2ForHer <roshile@gmail.com>
2026-08-25 22:51:39 -0400

    Register PeekPreviewPlugin in MainActivity

--- android/app/src/main/java/com/peekpreview/grok/MainActivity.java
+++ android/app/src/main/java/com/peekpreview/grok/MainActivity.java
@@ -1,5 +1,12 @@
 package com.peekpreview.grok;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(PeekPreviewPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
